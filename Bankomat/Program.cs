5f67ac73f6bd5bfbda91cc4837dd2c074cdb475a
tidr501 <tidr501@PC30441>
2024-10-01 10:46:34 +0200

    Add project files.

---
+++ Bankomat/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Bankomat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till USSR Bank!");
+            Console.WriteLine("Vänligen mata in Användarnamn: ");
+            Console.WriteLine("Vänligen mata in Lösenord: ");
+        }
+    }
+}
